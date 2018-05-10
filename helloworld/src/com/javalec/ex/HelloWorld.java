628ea2306ec0dd6f58c2d0adf29e1b58436b661b
Semin <cing9393@gmail.com>
2018-05-10 13:57:43 +0900

    Servlet 본격적으로 살펴보기 (1) - get,post 방식

--- helloworld/src/com/javalec/ex/HelloWorld.java
+++ helloworld/src/com/javalec/ex/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.javalec.ex;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-//@WebServlet("/HWorld")
+@WebServlet("/HWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -28,7 +30,20 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Hello World~");
+		System.out.println("doGet");
+		
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("HelloWorld~~~");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();		
 	}
 
 	/**
